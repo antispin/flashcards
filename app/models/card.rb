ceec4bd73f067e9fa4c953d58409b63fbf7a3ac2
Vadim <vadim-ip@mail.ru>
2016-08-15 13:46:41 +0400

    rubocop pass

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,4 @@
 class Card < ActiveRecord::Base
-
-  validates :original_text,   presence: true, length: { minimum: 5 }
-  validates :translated_text, presence: true, length: { minimum: 5 }
-
-
+  validates :original_text,   presence: true, length: { minimum: 1 }
+  validates :translated_text, presence: true, length: { minimum: 1 }
 end
